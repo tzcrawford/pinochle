1ffdc317ad18bcbbf2b7a64d90015a0cf84b5abe
tzcrawford <z@tzcrawford.com>
2023-10-29 15:38:34 -0700

    enhancement:server,client:Switched general procedural code to builder pattern objects.

--- client.py
+++ client.py
@@ -2,41 +2,71 @@
 import socket
 import server
 import json
+from warnings import warn
 
-CHARSET = server.CHARSET
-PORT = server.PORT
-SERVER_IP = server.SERVER_IP # This line will need to change
-SERVER_ADDR = server.SERVER_ADDR
-HEADER_SIZE = server.HEADER_SIZE
-DISCONNECT_MESSAGE = server.DISCONNECT_MESSAGE
+class Client:
 
-def start():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(SERVER_ADDR)
+    def __init__(self):
 
-    return client_socket
+        # Declare default attributes needed to run server so we can build it with connect()
+        self.CHARSET = server.DEFAULT_CHARSET
+        self.PORT = server.DEFAULT_PORT
+        self.SERVER_IP = server.DEFAULT_SERVER_IP # This line will need to change
+        self.HEADER_SIZE = server.DEFAULT_HEADER_SIZE
+        self.DISCONNECT_MESSAGE = server.DEFAULT_DISCONNECT_MESSAGE
+        self.connected = False
 
-def send(message_text, client_socket):
-    msg = dict() ; msg_header = dict() # We send communications as JSON, as converted from dictionary obj
-
-    # Declare what the message is
-    msg['text'] = message_text
-    msg = json.dumps(msg)
-    msg = msg.encode(CHARSET)
+    # Build the client object
+    def connect(self):
+        try:
+            self.SERVER_ADDR = (self.SERVER_IP, self.PORT)
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect(self.SERVER_ADDR)
+            self.connected = True
+        except Exception as e:
+            self.SERVER_ADDR = False
+            self.connected = False
+            warn(f"[CONNECTION FAILED]: {e}")
+        return
     
-    # Collect metadata on the message
-    msg_header['size'] = str(len(msg)) # Get the length of the main message JSON in bytes
-    msg_header = json.dumps(msg_header) # Convert that dictionary to JSON string
-    msg_header = msg_header.encode(CHARSET) # Convert that string to bytes
-    msg_header += ' '.encode(CHARSET) * (HEADER_SIZE - len(msg_header)) # Pad the header so it matches expected HEADER_SIZE
+    
+    def send(self, message_text):
+        
+        try:
+            assert self.connected == True
 
-    # Send the message metadata to the server as a header with expected size
-    client_socket.send(msg_header)
-    # Send the message to the server
-    client_socket.send(msg)
+            # We send communications as JSON, as converted from dictionary obj
+            msg = dict() ; msg_header = dict() 
+    
+            # Declare what the message is
+            msg['text'] = message_text
+            msg = json.dumps(msg)
+            msg = msg.encode(self.CHARSET)
+            
+            # Collect metadata on the message
+            msg_header['size'] = str(len(msg)) # Get the length of the main message JSON in bytes
+            msg_header = json.dumps(msg_header) # Convert that dictionary to JSON string
+            msg_header = msg_header.encode(self.CHARSET) # Convert that string to bytes
+            msg_header += ' '.encode(self.CHARSET) * (self.HEADER_SIZE - len(msg_header)) # Pad the header so it matches expected HEADER_SIZE
+    
+            # Send the message metadata to the server as a header with expected size
+            self.socket.send(msg_header)
+            # Send the message to the server
+            self.socket.send(msg)
+
+            return True
+        
+        except Exception as e:
+            warn(f"Failed to send message: {e}")
+            return False
+
+    def disconnect(self):
+        self.send(self.DISCONNECT_MESSAGE)
 
 
 if __name__ == '__main__':
-    client_socket = start()
-    send("This is a test message", client_socket)
-    send("This is a second test message", client_socket)
+    client = Client()
+    client.connect()
+    client.send("This is a test message")
+    client.send("This is a second test message")
+    client.disconnect()
--- server.py
+++ server.py
@@ -2,70 +2,81 @@
 import socket
 import threading
 import json
+from warnings import warn
 
-CHARSET='utf-8'
-PORT = 5050
-#SERVER_IP = "192.168.0.188"
-SERVER_IP = socket.gethostbyname(socket.gethostname()) #127.0.1.1 right now
-#print(server_ip)
-SERVER_ADDR = (SERVER_IP, PORT)
+DEFAULT_CHARSET='utf-8'
+DEFAULT_PORT = 5050
+#DEFAULT_SERVER_IP = "192.168.0.188"
+DEFAULT_SERVER_IP = socket.gethostbyname(socket.gethostname()) #127.0.1.1 right now
+DEFAULT_HEADER_SIZE = 64 # Length of metadata message we will accept from client tellling us more about the incoming message such as its size
+DEFAULT_DISCONNECT_MESSAGE = "DISCONNECT"
 
-HEADER_SIZE = 64 # Length of metadata message we will accept from client tellling us more about the incoming message such as its size
-DISCONNECT_MESSAGE = "DISCONNECT"
+class Server():
+    def __init__(self):
 
-# Here we handle messages from the connection with an individual client
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+        # Set base/default attributes needed to run server so we can build it with start()
+        self.CHARSET=DEFAULT_CHARSET
+        self.PORT = DEFAULT_PORT
+        self.SERVER_IP = DEFAULT_SERVER_IP
+        self.SERVER_ADDR = (self.SERVER_IP, self.PORT)
+        self.HEADER_SIZE = DEFAULT_HEADER_SIZE
+        self.DISCONNECT_MESSAGE = DEFAULT_DISCONNECT_MESSAGE
 
-    connected = True
-    while connected:
-        # First collect the message header
-        try:
-            msg_header = json.loads(conn.recv(HEADER_SIZE).decode(CHARSET))
-            msg_header['size'] = int(msg_header['size'])
-            
-            try: 
-                # Using information from the header, we collect the actual message
-                msg = json.loads(conn.recv(msg_header['size']).decode(CHARSET))
 
-                print(f"[{addr}]: {msg['text']}")
-
-                if msg['text'] == DISCONNECT_MESSAGE:
-                    connected = False
-
-            except Exception as e_msg:
-                print(f"Invalid Message from {addr}: ",e_msg)
+    # Here we listen for connections from new clients.
+    def start(self):
+        # Set up the socket
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Prevent the server crashing from restarts accessing the socket
+        server_socket.bind(self.SERVER_ADDR)
+        # Start listening for connections
+        server_socket.listen()
+        print(f"[LISTENING] Server is listening on {self.SERVER_IP}")
+        # Handle those connections as we listen
+        while True:
+            client_conn, client_addr = server_socket.accept()
+            thread = threading.Thread(target=self.handle_client, args=(client_conn, client_addr) )
+            print("Starting new thread for client at addr: ", client_addr)
+            thread.start()
+            print(f"Number of active connections: {threading.active_count() - 1}" )
+    
+    # Here we handle messages from the connection with an individual client
+    def handle_client(self, conn, addr):
+        print(f"[NEW CONNECTION] {addr} connected.")
+    
+        connected = True
+        while connected:
+            # First collect the message header
+            try:
+                msg_header = json.loads(conn.recv(self.HEADER_SIZE).decode(self.CHARSET))
+                msg_header['size'] = int(msg_header['size'])
+                
+                try: 
+                    # Using information from the header, we collect the actual message
+                    msg = json.loads(conn.recv(msg_header['size']).decode(self.CHARSET))
+    
+                    print(f"[{addr}]: {msg['text']}")
+    
+                    if msg['text'] == self.DISCONNECT_MESSAGE:
+                        connected = False
+    
+                except Exception as e_msg:
+                    warn(f"Invalid Message from {addr}: ",e_msg)
+                    continue
+            except Exception as e_header:
+                #warn(f"Invalid Message Header from {addr}:",e_header) # This line should not be enabled
+                    # This is expected to happen if the payload is not a header but rather the subsequent message
+                    # In other words, the contents of the loop will run for every client message sent, even if it is the actual message after the header. 
+                    # We should just ignore messages if they are not a header as they will be processed correctly if 
                 continue
-        except Exception as e_header:
-            #print(f"Invalid Message Header from {addr}:",e_header) # This line should not be enabled
-                # This is expected to happen if the payload is not a header but rather the subsequent message
-                # In other words, the contents of the loop will run for every client message sent, even if it is the actual message after the header. 
-                # We should just ignore messages if they are not a header as they will be processed correctly if 
-            continue
-
-    conn.close()
+    
+        conn.close()
 
 
-
-# Here we listen for connections from new clients.
-def start():
-    # Set up the socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Prevent the server crashing from restarts accessing the socket
-    server_socket.bind(SERVER_ADDR)
-    # Start listening for connections
-    server_socket.listen()
-    print(f"[LISTENING] Server is listening on {SERVER_IP}")
-    # Handle those connections as we listen
-    while True:
-        client_conn, client_addr = server_socket.accept()
-        thread = threading.Thread(target=handle_client, args=(client_conn, client_addr) )
-        print("Starting new thread for client at addr: ", client_addr)
-        thread.start()
-        print("Number of active connections: {}".format(threading.active_count() - 1) )
 
 
 if __name__ == '__main__':
     print("Starting Server...")
-    start()
+    server = Server()
+    server.start()
 
