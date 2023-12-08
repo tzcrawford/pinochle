09ab0b49ccea28d1bfd55bc2a62e2722c4e20ac5
tzcrawford <z@tzcrawford.com>
2023-12-07 22:36:22 -0700

    feature:frontend:Added a basic footer template.

--- src/SharedComponents/Footer.svelte
+++ src/SharedComponents/Footer.svelte
@@ -6,8 +6,21 @@ Footer : What the bottom of the web page will show
 -->
 
 <script lang="ts">
+    let gitRepo = "https://github.com/tzcrawford/pinochle"
 </script>
 
+<div>
+    <hr />
+    This project is distributed under a free and open source license.
+    <br />
+    <a href={gitRepo}>See the git repo for more information</a>
+</div>
 
 <style>
+    div {
+        text-align: center;
+    }
+    hr {
+        width: 80%;
+    }
 </style>
