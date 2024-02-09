fd238df24225729cfa0be662c38b15b4030d77fe
tzcrawford <z@tzcrawford.com>
2024-02-08 22:54:16 -0700

    bugfix:frontend:Fixed issue with CardStackTest which was preventing card hover due to vertical block elements partially overlaying other stacks. Resolution involved nesting the rotation transform div within the previous block element div.

--- src/Components/Game/CardStackTest.svelte
+++ src/Components/Game/CardStackTest.svelte
@@ -3,18 +3,34 @@ import CardStack from './CardStack.svelte'
 </script>
 
 <div class="card-stack-test" style="margin:2em">
-    <div style="display: flex; justify-content: center;">
-        <CardStack />
+    <div style="display: block">
+        <div style="display: flex; justify-content: center;">
+            <div style="transform:rotate(0);">
+                <CardStack />
+            </div>
+        </div>
     </div>
-    <div style="display: flex; justify-content: center;transform:rotate(90deg);">
-        <CardStack />
+    <div style="display: block">
+        <div style="display: flex; justify-content: center">
+            <div style="transform:rotate(90deg);">
+                <CardStack />
+            </div>
+        </div>
     </div>
     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-    <div style="display: flex; justify-content: center;transform:rotate(270deg);">
-        <CardStack />
+    <div style="display: block">
+        <div style="display: flex; justify-content: center">
+            <div style="transform:rotate(270deg);">
+                <CardStack />
+            </div>
+        </div>
     </div>
-    <div style="display: flex; justify-content: center;transform:rotate(45deg);">
-        <CardStack overlap="-4.8em" />
+    <div style="display: block">
+        <div style="display: flex; justify-content: center">
+            <div style="transform:rotate(45deg);">
+                <CardStack overlap="-4.8em" />
+            </div>
+        </div>
     </div>
     <br /><br /><br /><br /><br /><br /><br />
 </div>
