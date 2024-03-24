7c29d98f2230d7369929299d18e1b198d87208ce
T. Zack Crawford <z@tzcrawford.com>
2024-03-24 14:17:01 -0700

    feature:frontend Added GameBoardTest to try to align card stacks in position for four players on the screen. Currently have these four card stacks in rough position, but I need to rectify the top and bottom players sticking to the top or bottom of the screen. And left and right players need to be centered in the middle of the page vertically.

---
+++ src/Components/Game/GameBoardTest.svelte
@@ -0,0 +1,102 @@
+<!--
+GameBoard : Holds all game elements once entering the game
+-->
+
+<script lang="ts">
+import CardStack from './CardStack.svelte'
+</script>
+<div class="game-board-cont0">
+    <div class="game-board-cont1">
+        <div class="left-player0">
+            <div class="left-player1">
+                <div class="left-player2">
+                    <CardStack />
+                </div>
+            </div>
+        </div>
+        <div class="top-and-bottom-players0">
+            <div class="top-and-bottom-players1">
+                <div class="top-player1">
+                    <div class="top-player2">
+                        <CardStack />
+                    </div>
+                </div>
+                <div class="bottom-player1">
+                    <div class="bottom-player2">
+                        <CardStack />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="right-player0">
+            <div class="right-player1">
+                <div class="right-player2">
+                    <CardStack />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+div.game-board-cont0 {
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+}
+div.game-board-cont1 {
+    display: grid;
+    grid-template-columns: 20% 60% 20%;
+    width: 100%;
+    height: 100%;
+}
+
+div.left-player0 {
+    width: 30%;
+    height: 100%;
+}
+div.right-player0 {
+    width: 30%;
+    height: 100%;
+}
+div.top-and-bottom-players0 {
+    width: 40%;
+    height: 100%;
+}
+div.top-and-bottom-players1 {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    height: 100%;
+}
+
+
+div.left-player1 {
+    display: flex;
+    justify-content: center;
+}
+div.right-player1 {
+    display: flex;
+    justify-content: center;
+}
+
+div.left-player2 {
+    transform: rotate(90deg);
+}
+div.right-player2 {
+    transform: rotate(-90deg);
+}
+div.top-player1 {
+}
+div.bottom-player1 {
+}
+div.top-player2 {
+    display: flex;
+    justify-content: center;
+}
+div.bottom-player2 {
+    display: flex;
+    justify-content: center;
+}
+
+</style>
--- src/Components/MainPageContent.svelte
+++ src/Components/MainPageContent.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 import CardStackTest from './Game/CardStackTest.svelte'
+import GameBoardTest from './Game/GameBoardTest.svelte'
 </script>
 
-<CardStackTest />
+<!--CardStackTest /-->
+<GameBoardTest />
